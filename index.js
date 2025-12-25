15ceddd26b1b6776b6fea1ea7ad14877acb8c3c2
Jose Garcia <josegarcia19809@gmail.com>
2025-12-25 11:01:01 -0600

    DevTree - Creando nuestro Servidor en Express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hola Mundo");
+const express = require('express');
+const app = express();
+
+
+// Routing
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
+app.listen(4000, () => {
+    console.log("server is running on port " + 4000);
+})
