24737b71b253dee8eca015f0387d2a50bf16633d
Jose Garcia <josegarcia19809@gmail.com>
2026-01-02 17:21:29 -0600

    DevTree - Registro nuestro primer usuario en la base de datos

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,14 @@
 import {Router} from "express";
+import User from "./models/User.ts";
 
 const router = Router();
 
 
 // Routing
-router.post('/auth/register', (req, res) => {
-    res.send(req.body);
+router.post('/auth/register', async (req, res) => {
+    const user = new User(req.body);
+    await user.save();
+    res.send("Registro creado correctamente");
 })
 
 
