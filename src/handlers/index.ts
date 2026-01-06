3fa2c72c5458b1d9d7c0d10644f37c3827a5969a
Jose Garcia <josegarcia19809@gmail.com>
2026-01-06 11:55:07 -0600

    DevTree - Evitar Usuarios Duplicados

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -3,7 +3,15 @@ import type {Request, Response} from "express";
 import User from "../models/User.ts";
 
 export const createAccount = async (req: Request, res: Response) => {
+
+    const {email} = req.body;
+    const userExists = await User.findOne({email});
+    if (userExists) {
+        const error = new Error("El usuario ya existe");
+        return res.status(409).send({error: error.message});
+    }
+
     const user = new User(req.body);
     await user.save();
-    res.send("Registro creado correctamente");
+    res.status(200).send("Registro creado correctamente");
 }
