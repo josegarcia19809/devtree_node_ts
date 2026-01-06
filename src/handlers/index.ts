410e140f87e0f6b60beee3cafe85ab804f2d74ac
Jose Garcia <josegarcia19809@gmail.com>
2026-01-06 12:15:47 -0600

    DevTree - Hashear passwords de usuarios

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,10 @@
 import type {Request, Response} from "express";
-// @ts-ignore
 import User from "../models/User.ts";
+import {hashPassword} from '../utils/auth.ts';
 
 export const createAccount = async (req: Request, res: Response) => {
 
-    const {email} = req.body;
+    const {email, password} = req.body;
     const userExists = await User.findOne({email});
     if (userExists) {
         const error = new Error("El usuario ya existe");
@@ -12,6 +12,7 @@ export const createAccount = async (req: Request, res: Response) => {
     }
 
     const user = new User(req.body);
+    user.password = await hashPassword(password);
     await user.save();
     res.status(200).send("Registro creado correctamente");
 }
