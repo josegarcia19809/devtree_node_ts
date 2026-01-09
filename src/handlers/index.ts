d5dc4f49d624921f88a4351a8e626ef7bed1ae1d
Jose Garcia <josegarcia19809@gmail.com>
2026-01-09 08:37:09 -0600

    DevTree - Validación con Express Validator - Primeros Pasos

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,16 @@
 import type {Request, Response} from "express";
+import {validationResult} from "express-validator";
 import slug from "slug";
 import User from "../models/User.ts";
 import {hashPassword} from '../utils/auth.ts';
-import user from "../models/User.ts";
 
 export const createAccount = async (req: Request, res: Response) => {
+
+    // Manejar errores
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
 
     const {email, password} = req.body;
     const userExists = await User.findOne({email});
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,14 @@
 import {Router} from "express";
-// @ts-ignore
+import {body} from "express-validator";
 import {createAccount} from "./handlers/index.ts";
 
 const router = Router();
 
 
 // Routing
-router.post('/auth/register', createAccount)
+router.post('/auth/register',
+    body('handle').notEmpty(),
+    createAccount)
 
 
 export default router;
