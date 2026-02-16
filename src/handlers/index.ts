4edac0c934e696d5d8aad018315c1f4b36181e4f
Jose Garcia <josegarcia19809@gmail.com>
2026-02-16 17:36:35 -0600

    DevTree - Consultando la base de datos y mostrando la información

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -116,6 +116,12 @@ export const uploadImage = async (req: Request, res: Response) => {
 export const getUserByHandle = async (req: Request, res: Response) => {
     try {
         const {handle} = req.params;
+        const user = await User.findOne({handle}).select("-_id -__v -email -password");
+        if (!user) {
+            const error = new Error("El usuario no existe");
+            return res.status(404).json({error: error.message});
+        }
+        res.json({user: user});
 
     } catch (e) {
         const error = new Error("Hubo un error");
